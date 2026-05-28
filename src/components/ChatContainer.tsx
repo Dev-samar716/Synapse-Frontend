b465917395c77a4136f48b60435383d54fe220cc
Samar Kasaudhan <samardevworks@gmail.com>
2026-05-28 20:23:50 +0545

    Added an extra filter to only render existing messages

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,5 +1,4 @@
 
-
 import useContent from "../hooks/features/chat/useContent";
 import ChatMessage from "./ChatMessage";
 
@@ -11,9 +10,15 @@ const ChatContainer = () => {
   return (
     <main className="flex-1 overflow-y-auto px-6 py-8 bg-black">
       <div className="max-w-4xl mx-auto flex flex-col gap-6">
-        {contents.length > 0 && contents.map((content, index) => {
-           return <ChatMessage role={content.role} message={content.parts[0].text} key={index} />
-        })}
+        {contents
+  .filter(content => content.parts?.[0]?.text)   // Only showing messages with text
+  .map((content, index) => (
+    <ChatMessage
+      key={index}
+      role={content.role}
+      message={content.parts[0].text}
+    />
+  ))}
       </div>
     </main>
   );
