d82ae63eb3ea17ab9d83c48c4df5d760b58753fc
Samar Kasaudhan <samardevworks@gmail.com>
2026-05-28 21:45:08 +0545

    Removed side bar temporarily

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,6 +1,5 @@
 
 
-import Sidebar from "../components/SideBar";
 import Navbar from "../components/Navbar";
 import ChatContainer from "../components/ChatContainer";
 import InputBox from "../components/InputBox";
@@ -8,8 +7,6 @@ import InputBox from "../components/InputBox";
 const ChatPage = () => {
   return (
     <div className="w-full h-screen bg-black flex overflow-hidden">
-      {/* Sidebar */}
-      <Sidebar />
 
       {/* Main Layout */}
       <div className="flex-1 flex flex-col">
